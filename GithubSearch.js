0b2491b567b91d772e3a433d51bd9529892ddf4c
Gus <guspearson78@gmail.com>
2025-12-16 17:01:46 -0800

    Update GithubSearch.js

--- GithubSearch.js
+++ GithubSearch.js
@@ -1,10 +1,12 @@
   var search = document.getElementById("GHsearch");
   var tab = document.getElementById("link");
   var tab1 = document.getElementById("link1");
+const a = if(!(search === ("" || " "))) ? "https://api.github.com/search/users?q=" + search : undefined;
+   
     function submit(search){
         if(!(search === ("" || " "))) {
-            tab.href = "https://api.github.com/search/users?q=" + search; 
-            tab1.href = "https://api.github.com/search/users?q=" + search; 
+            tab.href = a;
+            tab1.href = a;
     
         }
         else {
@@ -14,8 +16,8 @@
   //for both: display results on page, doesn't switch tabs, meaning  DOESN'T WORK
   function RepoSub(search){
     if(!(search === ("" || " "))) {
-      tab.href = "https://api.github.com/search/repositories?q=" + search;
-      tab1.href = "https://api.github.com/search/users?q=" + search; 
+      tab.href = a;
+      tab1.href = a;
     }else {
       alert("Field cannot by whitespace or null!");
     }
